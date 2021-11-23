f6f7ab24c668a73734f886617c1d8c9b9fd09fe4
Connor Keith <keithcs@mail.uc.edu>
2021-11-22 21:52:44 -0500

    some more styling :)

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import NavbarStyles from '../styles/Navbar.module.css';
 import { FcDocument, FcFeedback } from 'react-icons/fc'
-import { BsLinkedin, BsTerminalFill } from 'react-icons/bs'
+import { BsLinkedin, BsTerminalFill, BsGithub, BsEnvelopeFill, BsFileTextFill } from 'react-icons/bs'
+
 
 function Navbar() {
+
+    const [selectedIcon, setSelectedIcon] = useState<string>("")
+
     return (
         <div className={NavbarStyles['container']}>
-            <FcDocument size={70} />
-            <FcFeedback size={70} />
-            <BsLinkedin size={70} />
-            <BsTerminalFill size={70} />
-            </div>
+            <span className={NavbarStyles['tooltip']}>{selectedIcon}</span>
+            <BsFileTextFill onMouseOver={() => setSelectedIcon("Resume")} size={70} color="#9eeaff" />
+            <BsEnvelopeFill onMouseOver={() => setSelectedIcon("Contact me")} size={70} color="#0094bd"/>
+            <BsLinkedin onMouseOver={() => setSelectedIcon("LinkedIn")} size={70} color="#2600ff"/>
+            <BsTerminalFill onMouseOver={() => setSelectedIcon("Interactive Terminal")} size={70}  />
+            <BsGithub onMouseOver={() => setSelectedIcon("GitHub")} size={70} color="#900fd1" />
+        </div>
     )
 }
 
--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ToolbarStyle from "../styles/Toolbar.module.css"
+import { BiHomeAlt } from 'react-icons/bi'
 
 function Toolbar() {
 
     return (
-        <div className={ToolbarStyle["toolbar"]}></div>
+        <div className={ToolbarStyle["toolbar"]}>
+            <BiHomeAlt size={20} color="white"/>
+        </div>
 
         // TODO: Home icon top left, other icons?
     )
